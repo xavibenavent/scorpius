2b66eb8e93539fdf64df7085c5dfe14d153b7c25
Xavier Benavent <xavibenavent@gmail.com>
2021-06-08 15:34:53 +0200

    First commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# main.py
+
+from test_class import STest
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = STest()
+    s.test_me()
---
+++ src/test_class.py
@@ -0,0 +1,8 @@
+# test_class.py
+
+class STest:
+    def __init__(self):
+        self.name = 'test'
+
+    def test_me(self):
+        return f'this is a {self.name}'
